495838091218cea9782723505e8f3b1c7f62950d
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2021-01-05 18:12:49 -0500

    v3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import artisanlogo from '../images/artisan-logo.png';
+import avinkalogo from '../images/avinka-logo.png';
 
 
 function Header() {
   return (
       <div className="header">
-      <h1>
-        Bienvenidos a la Ruleta Artisan<br /> BY Avinka
-      </h1>
-      {/* <img src={artisan} classname="artisan" alt="logo-artisan" /> */}
+        <h1>Bienvenidos a la ruleta</h1>
+        <img src={artisanlogo} classname="artisan" alt="logo-artisan" />
+        <div className ="avinkaHeader">
+          <h1>BY</h1>
+          <img src={avinkalogo} alt="logo-avinka" />
+        </div>
       </div>
   )
 }
