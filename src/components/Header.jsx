4ad393babd936a9c5ab5ce9c0b44d2295d44403f
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2021-01-06 15:06:40 -0500

    cambios en colores ruleta

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import artisanlogo from '../images/artisan-logo.png';
-import avinkalogo from '../images/avinka-logo.png';
+// import avinkalogo from '../images/avinka-logo.png';
 
 
 function Header() {
@@ -8,10 +8,10 @@ function Header() {
       <div className="header">
         <h1>Bienvenidos a la ruleta</h1>
         <img src={artisanlogo} classname="artisan" alt="logo-artisan" />
-        <div className ="avinkaHeader">
+        {/* <div className ="avinkaHeader">
           <h1>BY</h1>
           <img src={avinkalogo} alt="logo-avinka" />
-        </div>
+        </div> */}
       </div>
   )
 }
