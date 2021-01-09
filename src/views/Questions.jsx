d8c6d75407ba2a899d9dc69f3fa6bcc5d96cfe4a
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2021-01-08 21:41:19 -0500

    cambios de diseño en preguntas

--- src/views/Questions.jsx
+++ src/views/Questions.jsx
@@ -15,42 +15,42 @@ const Questions = () =>{
 		{   category:"categoria1",
 			questionText: 'Artisan es:',
 			answerOptions: [
-				{ answerText: 'Nueva línea de pavos', isCorrect: false },
-				{ answerText: 'Nueva línea de pollos con crianza libre de antibióticos', isCorrect: true },
-				{ answerText: 'Nueva línea de precocidos', isCorrect: false },
+				{ answerText: 'A. Nueva línea de pavos', isCorrect: false },
+				{ answerText: 'B. Nueva línea de pollos con crianza libre de antibióticos', isCorrect: true },
+				{ answerText: 'C. Nueva línea de precocidos', isCorrect: false },
 			],
 		},
 		{   category:"categoria2",
 			questionText: 'La actriz del comercial de Artisan es:',
 			answerOptions: [
-				{ answerText: 'Karen Schwarz', isCorrect: false },
-				{ answerText: 'Maria Pia Copello', isCorrect: false },
-				{ answerText: 'Chiara Pinasco', isCorrect: true },
+				{ answerText: 'A. Karen Schwarz', isCorrect: false },
+				{ answerText: 'B. Maria Pia Copello', isCorrect: false },
+				{ answerText: 'C. Chiara Pinasco', isCorrect: true },
 			],
 		},
 		{   category:"categoria3",
 			questionText: 'Artisan viene con empaques:',
 			answerOptions: [
-				{ answerText: 'Simples', isCorrect: false },
-				{ answerText: 'Con tecnología de atmosfera modificada', isCorrect: true },
-				{ answerText: 'Cartón', isCorrect: false },
+				{ answerText: 'A. Simples', isCorrect: false },
+				{ answerText: 'B. Con tecnología de atmosfera modificada', isCorrect: true },
+				{ answerText: 'C. Cartón', isCorrect: false },
 			],
 		},
 		{   category:"categoria4",
 			questionText: 'Artisan se vende en:',
 			answerOptions: [
-				{ answerText: 'Supermercados', isCorrect: false },
-				{ answerText: 'Tiendas Avinka', isCorrect: false },
-				{ answerText: 'Delivery Avinka', isCorrect: false },
-				{ answerText: 'Todas las anteriores', isCorrect: true },
+				{ answerText: 'A. Supermercados', isCorrect: false },
+				{ answerText: 'B. Tiendas Avinka', isCorrect: false },
+				{ answerText: 'C. Delivery Avinka', isCorrect: false },
+				{ answerText: 'D. Todas las anteriores', isCorrect: true },
 			],
 		},
 		{   category:"categoria5",
 			questionText: 'Artisan es una marca de:',
 			answerOptions: [
-				{ answerText: 'Avinka', isCorrect: true },
-				{ answerText: 'Santa Elena', isCorrect: false },
-				{ answerText: 'N/A', isCorrect: false },
+				{ answerText: 'A. Avinka', isCorrect: true },
+				{ answerText: 'B. Santa Elena', isCorrect: false },
+				{ answerText: 'C. N/A', isCorrect: false },
 			],
 		},
 	];
@@ -75,11 +75,12 @@ const Questions = () =>{
 		</div>
 		<div className='app'>
 			<div className='question-section'>
-				<div className='question-text'>{pregunta.questionText}</div>
+				<div className='question-text'><p>{pregunta.questionText}</p></div>
 				<div className='question-count'>
 					<img src={marker} className="artisan-logo" alt="marker" />
 				</div>
 			</div>
+			<div className="texto-pregunta"><p>Seleciones una respuesta:</p></div>
 			<div className='answer-section'>
 				{pregunta.answerOptions.map((answerOption) => (
 					<Link classname="gopage" to={answerOption.isCorrect? "/winner" : "/loser"}>
